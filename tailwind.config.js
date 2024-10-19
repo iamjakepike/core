81df6fafa7acde62252699520310669f47647a18
Jake Pike <me@jakepike.co.uk>
2024-10-20 00:02:15 +0100

    Setup The Ability to set variables for fonts, body text and bodt bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 module.exports = {
-    content: require('fast-glob').sync(
-      ["./**/*.{html,php}"],
-    ),
-    safelist: [
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: []
-  }
-  
+	content: require("fast-glob").sync(["./**/*.{html,php}"]),
+	safelist: [],
+	theme: {
+		extend: {
+			colors: {
+				black: "var(--black)",
+				white: "var(--white)",
+				"core-bg": "var(--core-bg)",
+				"core-text": "var(--core-text)",
+			},
+			fontFamily: {
+				sans: "var(--default-sans)",
+				serif: "var(--default-serif)",
+			},
+		},
+	},
+	plugins: [],
+};
